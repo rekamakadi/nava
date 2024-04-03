6caa8e0f563fa8ee5712cc60891b057cc1089002
rekamakadi <r.makadi@gmail.com>
2024-04-03 23:42:03 +0200

    [feat] add basic style to ImageList

--- client/src/components/ImageList.jsx
+++ client/src/components/ImageList.jsx
@@ -1,29 +1,39 @@
 import React from "react";
 import { Accordion, Card, Button } from "react-bootstrap";
+import './ImageList.css';
 
 const ImageList = ({ images }) => {
   return (
     <Accordion defaultActiveKey="0">
-      {images.map((image, index) => (
-      <Accordion.Item eventKey={index} key={index}>
-        <Accordion.Header>
-          <img
-            src={`https://picsum.photos/id/${index}/200/300`}
-            alt={`Kép ${image.filename_str}`}
-            style={{ marginRight: '10px', width: '50px', height: 'auto' }}
-          />
-          {`${image.filename_str} - ${image.description_str}`}
-        </Accordion.Header>
-        <Accordion.Body>
-          <p>{`Azonosító: ${image.id}`}</p>
-          <p>{`Méret: ${image.ow_i}x${image.oh_i}`}</p>
-          <p>{`Készítés dátuma: ${image.createDate_dt}`}</p>
-          <p>{`Módosítás dátuma: ${image.harvestDate_dt}`}</p>
-          <p>{`Formátum: ${image.format_str}`}</p>
-          <Button href={`/${image.id}`}>Metaadatok</Button>
-        </Accordion.Body>
-      </Accordion.Item>
-      ))}
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+        {images.map((image, index) => (
+          <Accordion.Item eventKey={index} key={index}>
+            <Accordion.Header>
+              <div className="d-flex align-items-center p-2 rounded custom-hover">
+                <img
+                  src={`https://picsum.photos/id/${index}/80`}
+                  alt={`Kép ${image.filename_str}`}
+                  className="me-3 img-thumbnail"
+                />
+                <div className="flex-grow-1">
+                  <h6 className="mb-0">{image.filename_str}</h6>
+                  <p className="mb-0">{image.description_str}</p>
+                </div>
+              </div>
+            </Accordion.Header>
+            <Accordion.Body>
+              <div className="mb-3">
+                <p className="mb-0"><strong>Azonosító:</strong> {image.id}</p>
+                <p className="mb-0"><strong>Méret:</strong> {`${image.ow_i}x${image.oh_i}`}</p>
+                <p className="mb-0"><strong>Készítés dátuma:</strong> {image.createDate_dt}</p>
+                <p className="mb-0"><strong>Módosítás dátuma:</strong> {image.harvestDate_dt}</p>
+                <p className="mb-0"><strong>Formátum:</strong> {image.format_str}</p>
+              </div>
+              <Button href={`/${image.id}`} variant="primary">Metaadatok</Button>
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </div>
     </Accordion>
   );
 }

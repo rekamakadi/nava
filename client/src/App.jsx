dd49eada2c6440640643830ec1b0a8d12790dea4
rekamakadi <r.makadi@gmail.com>
2024-04-03 02:04:02 +0200

    [feat] fetch data + implement routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,35 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import ImageList from './components/ImageList';
+import ImageData from './components/ImageData';
+import ImageDataEditor from './components/ImageDataEditor';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
+  const [images, setImages] = useState([]);
+  
+  function fetchImages() {
+    fetch("http://localhost:5000")
+    .then((res) => !res.ok ? Error("Unexpected server response") : res.json())
+    .then((data) => setImages(JSON.parse(data).response.docs))
+    .catch((error) => console.log("Error: ", error));
+  }
+
+  useEffect(() => fetchImages(), []);
+
   return (
-    <div>
-      <NavigationBar/>
-    </div>
+    <>
+      <BrowserRouter>
+        <NavigationBar/>
+        <Routes>
+          <Route path="/" element={<ImageList images={images}/>}/>
+          <Route path="/:id" element={<ImageData/>}/>
+          <Route path="/update/:id" element={<ImageDataEditor/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 

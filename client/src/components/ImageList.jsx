20039d628978da99253d3ed4e56cabb621684602
rekamakadi <r.makadi@gmail.com>
2024-04-03 02:03:21 +0200

    [feat] implement ImageList.jsx

--- client/src/components/ImageList.jsx
+++ client/src/components/ImageList.jsx
@@ -1,9 +1,30 @@
-const ImageList = () => {
+import React from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
+
+const ImageList = ({ images }) => {
   return (
-    <div>
-      
-    </div>
-  )
+    <Accordion defaultActiveKey="0">
+      {images.map((image, index) => (
+      <Accordion.Item eventKey={index} key={index}>
+        <Accordion.Header>
+          <img
+            src={`https://picsum.photos/id/${index}/200/300`}
+            alt={`Kép ${index}`}
+            style={{ marginRight: '10px', width: '50px', height: 'auto' }}
+          />
+          {`Kép ${index + 1} - Leírás: ${image.description_str}`}
+        </Accordion.Header>
+        <Accordion.Body>
+          <p>{`ID: ${image.id}`}</p>
+          <p>{`Méret: ${image.ow_i}x${image.oh_i}`}</p>
+          <p>{`Készítés dátuma: ${image.createDate_dt}`}</p>
+          <p>{`Módosítás dátuma: ${image.harvestDate_dt}`}</p>
+          <p>{`Formátum: ${image.format_str}`}</p>
+        </Accordion.Body>
+      </Accordion.Item>
+      ))}
+    </Accordion>
+  );
 }
 
-export default ImageList
+export default ImageList;

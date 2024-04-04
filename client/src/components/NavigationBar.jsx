0a9c714b1537cbc4ee760bc50a56700152c8b269
rekamakadi <r.makadi@gmail.com>
2024-04-04 05:20:12 +0200

    [change] set back not fixed

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -8,7 +8,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark" fixed="top">
+    <Navbar expand="lg" bg="dark" variant="dark">
       <Container fluid>
         <Navbar.Brand href="/"><img src="..\src\assets\nava logo.svg" alt="Logó" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarNav" />
